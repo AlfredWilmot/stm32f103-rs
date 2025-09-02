65946a9bd2edbd9f62866a2b7c68c761da4afec4
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2025-09-02 20:44:57 +0100

    refactor: some QoL enhancements

--- up.sh
+++ up.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+# Setup and enter a docker container that has access to a connected STLINK
+
 STM32_IMG="stm32:main"
-USB_PATH="$(lsusb | grep ST-LINK | awk '{print "/dev/bus/usb/"$2"/"$4}' | tr -d ':')"
+USB_PATH="$(lsusb | grep ST-LINK | awk '{print "/dev/bus/usb/"$2"/"$4}' | tr -d ':'| sed -n '1p')"
 
 if ! docker images --format '{{.Repository}}:{{.Tag}}' | grep -q "${STM32_IMG}"; then
   docker build -t "${STM32_IMG}" .
 fi
 
-set -x
-#docker run --rm -it --device="${USB_PATH}" --privileged --cap-add SYS_ADMIN "${@}" stm32:main /bin/bash
-docker run --rm -it --device="${USB_PATH}" "${STM32_IMG}" "${@}" /bin/bash
+#CMD="docker run --rm -it ${*}"
+CMD="docker run --rm -it --privileged --cap-add SYS_ADMIN ${*}"
+if [ -n "${USB_PATH}" ]; then
+  CMD+=" --device=${USB_PATH} --env USB_PATH=${USB_PATH}"
+fi
+
+CMD+=" ${STM32_IMG} /bin/bash"
+
+echo "${CMD}"
+eval "${CMD}"
