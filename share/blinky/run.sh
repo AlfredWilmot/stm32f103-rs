3aa175daecbe4d52adbebbfbf83324b35bb23256
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2025-09-05 23:32:22 +0100

    feature: script to build/convert ELF into binary then flash to HW

---
+++ share/blinky/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJ=blinky
+TOOLCHAIN=thumbv7m-none-eabi
+BIN="${PROJ}.bin"
+ELF_PATH="target/${TOOLCHAIN}/release/${PROJ}"
+
+cargo build --release
+arm-none-eabi-objcopy -O binary "${ELF_PATH}" "${BIN}"
+st-flash write "${BIN}" 0x08000000
