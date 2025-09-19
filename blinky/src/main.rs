7a868746b893f38a98d74297381713645af48962
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2025-09-19 01:07:36 +0100

    wip: attempting to impl usb-serial using hal crate example

--- blinky/src/main.rs
+++ blinky/src/main.rs
@@ -23,31 +23,51 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::asm::delay;
 use panic_halt as _;
 
 use nb::block;
 
 use cortex_m_rt::entry;
+use stm32f1xx_hal::usb::{Peripheral};
 use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use usb_device::prelude::*;
+use stm32_usbd::{UsbBus};
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 #[entry]
 fn main() -> ! {
     // Get access to the core peripherals from the cortex-m crate
     let cp = cortex_m::Peripherals::take().unwrap();
+
     // Get access to the device specific peripherals from the peripheral access crate
     let dp = pac::Peripherals::take().unwrap();
-
-    let rcc = dp.RCC.constrain();
     let mut flash = dp.FLASH.constrain();
+    let rcclk = dp.RCC.constrain();
+    let clks = rcclk.cfgr.freeze(&mut flash.acr);
 
-    // Acquire the GPIOC peripheral
+    // Acquire the GPIOC peripheral, and configure GPIOC pin 13 as a push-pull output.
     let mut gpioc = dp.GPIOC.split();
-
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &rcc.cfgr.freeze(&mut flash.acr)).counter_hz();
+    led.set_high(); // turn off LED
+
+    let mut gpioa = dp.GPIOA.split();
+    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+    usb_dp.set_low();  // send RESET to USB bus by turning D+ pin on
+                       // (needed to reset device when new firmware is flashed)
+    delay(clks.sysclk().raw() / 100);
+
+    let usb = Peripheral {
+        usb: dp.USB,
+        pin_dm: gpioa.pa11,
+        pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
+    };
+
+    let usb_bus = UsbBus::new(usb);
+
+    let serial = usbd_serial::SerialPort::new(&usb_bus);
+
+    let mut timer = Timer::syst(cp.SYST, &clks).counter_hz();
     timer.start(10.Hz()).unwrap();
 
     // Wait for the timer to trigger an update and change the state of the LED
